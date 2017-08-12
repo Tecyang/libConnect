1a9f045fa721e5a997055e8a216e8b6f51055ca8
tecyang <me@tecyang.com>
2017-08-12 16:35:48 +0800

    建立项目

---
+++ Win32/LibConnect/Global.h
@@ -0,0 +1,26 @@
+
+#ifndef MAC_ADDR_LEN
+#define MAC_ADDR_LEN 6
+#endif
+
+#ifndef IP_ADDR_LEN
+#define IP_ADDR_LEN 4
+#endif
+
+
+
+#define ERROR_PCAP_FINDALLDEVS -1 //Error in pcap_findalldevs
+#define ERROR_PCAP_NOTINSTALL -2 //Error winpcap not install
+#define ERROR_INIT_FAIL -3 //init function fail
+
+
+#define PACKAGE_MAX_LEN 1024 //最大包长
+
+#pragma pack(push,1) //开始定义数据包, 采用字节对齐方式
+/*----------------------包头---------------------*/
+typedef struct tagPACKAGEHEAD
+{
+ WORD Command; //命令 
+ WORD nDataLen;//包体的长度
+}PACKAGE_HEAD;
+#pragma pack(pop) //结束定义数据包, 恢复原来对齐方式
